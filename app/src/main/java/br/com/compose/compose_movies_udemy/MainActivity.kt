a7828765b3f55fc3a73eb3eec18f8d5c519ea7ec
santosjoao <santosjoao@brq.com>
2023-06-02 13:40:52 -0300

    Adicionando Splash

--- app/src/main/java/br/com/compose/compose_movies_udemy/MainActivity.kt
+++ app/src/main/java/br/com/compose/compose_movies_udemy/MainActivity.kt
@@ -1,6 +1,8 @@
 package br.com.compose.compose_movies_udemy
 
 import android.os.Bundle
+import android.view.View
+import android.view.ViewTreeObserver
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.ExperimentalAnimationApi
@@ -8,15 +10,25 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
 import br.com.compose.compose_movies_udemy.navigation.AppNavigation
+import br.com.compose.compose_movies_udemy.presentation.HomeViewModel
 import br.com.compose.compose_movies_udemy.ui.theme.MoviesComposeTheme
 import com.google.accompanist.navigation.animation.rememberAnimatedNavController
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : ComponentActivity() {
 
+    private val viewModel : HomeViewModel by viewModel()
+    var counter = 0
     @OptIn(ExperimentalAnimationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
+        delaySplash()
         setContent {
             MoviesComposeTheme {
                 Surface(
@@ -27,6 +39,25 @@ class MainActivity : ComponentActivity() {
                     AppNavigation(navController = navController)
                 }
             }
+        }
+
+        val content: View = findViewById(android.R.id.content)
+        content.viewTreeObserver.addOnPreDrawListener(
+            object : ViewTreeObserver.OnPreDrawListener {
+                override fun onPreDraw(): Boolean {
+                    return if (counter == 1) {
+                        content.viewTreeObserver.removeOnPreDrawListener(this)
+                        true
+                    } else { false }
+                }
+            }
+        )
+    }
+
+    private fun delaySplash() {
+        lifecycleScope.launch {
+            delay(2000)
+            counter = 1
         }
     }
 }
