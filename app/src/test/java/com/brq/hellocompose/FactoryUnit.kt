1ecd2a2ead50a82012031e21cb06b18610503f45
joaosantos <joaomarcelo.ms@gmail.com>
2023-06-12 08:51:22 -0300

    Adicionando Uistate tests login

---
+++ app/src/test/java/com/brq/hellocompose/FactoryUnit.kt
@@ -0,0 +1,6 @@
+package com.brq.hellocompose
+
+const val VALID_NAME = "name"
+const val INVALID_PASS = "abc"
+const val VALID_PASS = "abcd12324"
+const val CORRECT_PASS = "abc123"
---
+++ app/src/test/java/com/brq/hellocompose/presentation/login/LoginViewModelTest.kt
@@ -0,0 +1,53 @@
+package com.brq.hellocompose.presentation.login
+
+import app.cash.turbine.test
+import com.brq.hellocompose.CORRECT_PASS
+import com.brq.hellocompose.INVALID_PASS
+import com.brq.hellocompose.MainDispatcherRule
+import com.brq.hellocompose.VALID_NAME
+import com.brq.hellocompose.VALID_PASS
+import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertFalse
+import junit.framework.TestCase.assertTrue
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class LoginViewModelTest {
+
+    private lateinit var viewModel: LoginViewModel
+
+
+    @get:Rule
+    val mainDispatcherRule = MainDispatcherRule()
+
+    @Before
+    fun setUp() {
+        viewModel = LoginViewModel()
+    }
+
+    @Test
+    fun `testing UI states from Login TRIGGERED BY EVENT`() = runTest {
+        viewModel.uiSTate.test {
+            val initialState = awaitItem()
+            assertEquals(initialState, LoginUiStates.Empty)
+            viewModel.onEvent(LoginEvent.ValidateNameField(VALID_NAME))
+            val result = awaitItem()
+            assertEquals("name", result.name)
+            viewModel.onEvent(LoginEvent.ValidatePassField(VALID_PASS))
+            val correctPass = awaitItem()
+            assertFalse(correctPass.isPassError)
+            viewModel.onEvent(LoginEvent.ValidatePassField(INVALID_PASS))
+            val resultWrongPass = awaitItem()
+            assertTrue(resultWrongPass.isPassError)
+            viewModel.onEvent(LoginEvent.ValidatePassField(CORRECT_PASS))
+            val correctPassAgain = awaitItem()
+            assertFalse(correctPassAgain.isPassError)
+            viewModel.onEvent(LoginEvent.ValidateLogin)
+            val resultFinal = awaitItem()
+            assertTrue(resultFinal.isSuccessLogin)
+        }
+    }
+
+}
