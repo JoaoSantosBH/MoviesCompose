2f130c5624fd90e9c7911228071670ab6ea26ba5
João Santos <joaomarcelo.ms@gmail.com>
2023-06-30 11:20:06 -0300

    Ajustes Novo layout Figma: Fonte Nunito no theme

--- app/src/main/java/com/brq/hellocompose/ui/theme/Theme.kt
+++ app/src/main/java/com/brq/hellocompose/ui/theme/Theme.kt
@@ -17,6 +17,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
+
 private val DarkColorScheme = darkColorScheme(
     background = BackgroundDark,
     surface = SurfaceDark,
